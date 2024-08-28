0d2ac56cee4e18662b439a863ffee090cff33894
YoungHyoKim90 <younghyo1101@gmail.com>
2024-08-28 14:17:25 +0900

    login complete

--- loginServer/src/com/example/LoginServlet.java
+++ loginServer/src/com/example/LoginServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("/login")
+@WebServlet("/index")
 public class LoginServlet extends HttpServlet {
 
     @Override
@@ -24,6 +24,22 @@ public class LoginServlet extends HttpServlet {
             resp.sendRedirect("home.jsp");
         } else {
             resp.sendRedirect("error.jsp");
+        }
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // GET 요청 처리
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // 기본적인 사용자 인증 로직 (실제 프로젝트에서는 데이터베이스 사용 권장)
+        if ("user".equals(username) && "password".equals(password)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            response.sendRedirect("home.jsp");
+        } else {
+        	response.sendRedirect("error.jsp");
         }
     }
 }
