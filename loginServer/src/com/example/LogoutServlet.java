74aad821cd62939a691d91f93e17282fb3c2404a
YoungHyoKim90 <younghyo1101@gmail.com>
2024-08-30 09:49:56 +0900

    LogoutServlet

--- loginServer/src/com/example/LogoutServlet.java
+++ loginServer/src/com/example/LogoutServlet.java
@@ -28,3 +28,10 @@ public class LogoutServlet extends HttpServlet {
         doPost(request, response);
     }
 }
+
+/*
+@WebServlet("/logout"): 이 어노테이션은 서블릿이 /logout 경로에서 요청을 처리하도록 설정합니다.
+doPost: POST 요청을 처리합니다. 세션을 가져와 무효화하고, login.jsp로 리디렉션합니다.
+doGet: GET 요청을 처리할 때도 POST와 동일한 동작을 수행하도록 합니다.
+이 서블릿을 배포 후, 로그아웃 링크 클릭 시 logout URL로 요청이 전송되며, 사용자는 login.jsp로 리디렉션됩니다.
+*/
