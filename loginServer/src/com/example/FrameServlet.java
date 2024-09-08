d7dc5d1310663633c3225a8ee1011f9bd86c7d8d
YoungHyoKim90 <younghyo1101@gmail.com>
2024-09-08 16:48:59 +0900

    FrameServlet

--- loginServer/src/com/example/FrameServlet.java
+++ loginServer/src/com/example/FrameServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 /**
  * 
  * @author NNPC999
@@ -13,21 +14,21 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/frame")
 public class FrameServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // 요청을 JSP 페이지로 포워딩
-        request.getRequestDispatcher("/frame.jsp").forward(request, response);
-    }
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 요청을 JSP 페이지로 포워딩
+		request.getRequestDispatcher("/frame.jsp").forward(request, response);
+	}
 
-    
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // GET 요청 처리와 동일하게 동작하도록 설정
-        doGet(request, response);
-        
-    }
-    
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// GET 요청 처리와 동일하게 동작하도록 설정
+		doGet(request, response);
+
+	}
+
 }
-
